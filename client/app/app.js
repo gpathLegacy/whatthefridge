1335182ada060282694829b6fff83150841e9a65
Jbays <jjbaize@gmail.com>
2015-09-23 12:21:37 -0500

    remove in-line comments

--- client/app/app.js
+++ client/app/app.js
@@ -15,16 +15,13 @@ angular.module('wtf', [
 
 .config(function($routeProvider, $httpProvider) {
 	$routeProvider
-		//about
 		.when('/about', {
 			templateUrl: 'app/about/about.html'
 		})
-		//background
 		.when('/', {
 			templateUrl: 'app/auth/landing.html',
 			controller: 'BackgroundController'
 		})
-		//auth
 		.when('/login', {
 			templateUrl: 'app/auth/login.html',
 			controller: 'AuthController'
@@ -40,22 +37,18 @@ angular.module('wtf', [
 				Auth.signout();
 			}}
 		})
-		//create-recipes
 		.when('/create-recipes', {
 			templateUrl: 'app/create-recipes/create-recipes.html',
 			controller: 'CreateRecipesController'
 		})
-		//dashboard
 		.when('/dashboard', {
 			templateUrl: 'app/dashboard/dashboard.html',
 			controller: 'DashboardController'
 		})
-		//edit-recipes
 		.when('/edit-recipes', {
 			templateUrl: 'app/edit-recipes/edit-recipes.html',
 			controller: 'EditRecipesController'
 		})
-		//shopping-list
 		when('/shopping-list', {
 			templateUrl: 'app/shopping-list/shopping-list.html',
 			controller: 'ShoppingListController'
