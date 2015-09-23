6805f5c345c0159f9a1afca443258d1dad644402
Jbays <jjbaize@gmail.com>
2015-09-23 12:04:53 -0500

    configure routes

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,9 @@
 angular.module('wtf', [
+	'ngRoute',
+	'ui.router',
 	'wtf.about',
 	'wtf.auth',
+	'wtf.background',
 	'wtf.create-recipes',
 	'wtf.dashboard',
 	'wtf.edit-recipes',
@@ -10,23 +13,18 @@ angular.module('wtf', [
 	]
 )
 
-//about
-//auth
-//create-recipes
-//dashboard
-//edit-recipes
-//services
-//shopping-list
-
 .config(function($routeProvider, $httpProvider) {
 	$routeProvider
+		//about
 		.when('/about', {
 			templateUrl: 'app/about/about.html'
 		})
+		//background
 		.when('/', {
 			templateUrl: 'app/auth/landing.html',
-			controller: 'AuthController'
+			controller: 'BackgroundController'
 		})
+		//auth
 		.when('/login', {
 			templateUrl: 'app/auth/login.html',
 			controller: 'AuthController'
@@ -35,27 +33,32 @@ angular.module('wtf', [
 			templateUrl: 'app/auth/signup.html',
 			controller: 'AuthController'
 		})
-		.when('/create-recipes', {
-			templateUrl: 'app/create-recipes/create-recipes.html',
-			controller: 'CreateRecipesController'
-		})
-		.when('/edit-recipes', {
-			templateUrl: 'app/edit-recipes/edit-recipes.html',
-			controller: 'EditRecipesController'
-		})
-		when('/shopping-list', {
-			templateUrl: 'app/shopping-list/shopping-list.html',
-			controller: 'ShoppingListController'
-		})
 		.when('/logout', {
 			templateUrl: 'app/auth/landing.html',
 			controller: 'AuthController',
 			resolve: { function(Auth) {
 				Auth.signout();
-			}
-			}
+			}}
+		})
+		//create-recipes
+		.when('/create-recipes', {
+			templateUrl: 'app/create-recipes/create-recipes.html',
+			controller: 'CreateRecipesController'
+		})
+		//dashboard
+		.when('/dashboard', {
+			templateUrl: 'app/dashboard/dashboard.html',
+			controller: 'DashboardController'
+		})
+		//edit-recipes
+		.when('/edit-recipes', {
+			templateUrl: 'app/edit-recipes/edit-recipes.html',
+			controller: 'EditRecipesController'
+		})
+		//shopping-list
+		when('/shopping-list', {
+			templateUrl: 'app/shopping-list/shopping-list.html',
+			controller: 'ShoppingListController'
 		})
 		.otherwise('/')
-
-
 });
