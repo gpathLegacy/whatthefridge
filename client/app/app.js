76ccc6fbbb19cb3b5f1a153507f3409c080a9ef3
Jbays <jjbaize@gmail.com>
2015-09-23 14:41:30 -0500

    change tab to spaces

--- client/app/app.js
+++ client/app/app.js
@@ -1,57 +1,57 @@
 angular.module('wtf', [
-	'ngRoute',
-	'ui.router',
-	'wtf.about',
-	'wtf.auth',
-	'wtf.background',
-	'wtf.create-recipes',
-	'wtf.dashboard',
-	'wtf.edit-recipes',
-	'wtf.index',
-	'wtf.services',
-	'wtf.shopping-list'
-	]
+  'ngRoute',
+  'ui.router',
+  'wtf.about',
+  'wtf.auth',
+  'wtf.background',
+  'wtf.create-recipes',
+  'wtf.dashboard',
+  'wtf.edit-recipes',
+  'wtf.index',
+  'wtf.services',
+  'wtf.shopping-list'
+  ]
 )
 
 .config(function($routeProvider, $httpProvider) {
-	$routeProvider
-		.when('/about', {
-			templateUrl: 'app/about/about.html'
-		})
-		.when('/', {
-			templateUrl: 'app/auth/landing.html',
-			controller: 'BackgroundController'
-		})
-		.when('/login', {
-			templateUrl: 'app/auth/login.html',
-			controller: 'AuthController'
-		})
-		.when('/signup', {
-			templateUrl: 'app/auth/signup.html',
-			controller: 'AuthController'
-		})
-		.when('/logout', {
-			templateUrl: 'app/auth/landing.html',
-			controller: 'AuthController',
-			resolve: { function(Auth) {
-				Auth.signout();
-			}}
-		})
-		.when('/create-recipes', {
-			templateUrl: 'app/create-recipes/create-recipes.html',
-			controller: 'CreateRecipesController'
-		})
-		.when('/dashboard', {
-			templateUrl: 'app/dashboard/dashboard.html',
-			controller: 'DashboardController'
-		})
-		.when('/edit-recipes', {
-			templateUrl: 'app/edit-recipes/edit-recipes.html',
-			controller: 'EditRecipesController'
-		})
-		when('/shopping-list', {
-			templateUrl: 'app/shopping-list/shopping-list.html',
-			controller: 'ShoppingListController'
-		})
-		.otherwise('/')
+  $routeProvider
+    .when('/about', {
+      templateUrl: 'app/about/about.html'
+    })
+    .when('/', {
+      templateUrl: 'app/auth/landing.html',
+      controller: 'BackgroundController'
+    })
+    .when('/login', {
+      templateUrl: 'app/auth/login.html',
+      controller: 'AuthController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
+    })
+    .when('/logout', {
+      templateUrl: 'app/auth/landing.html',
+      controller: 'AuthController',
+      resolve: { function(Auth) {
+        Auth.signout();
+      }}
+    })
+    .when('/create-recipes', {
+      templateUrl: 'app/create-recipes/create-recipes.html',
+      controller: 'CreateRecipesController'
+    })
+    .when('/dashboard', {
+      templateUrl: 'app/dashboard/dashboard.html',
+      controller: 'DashboardController'
+    })
+    .when('/edit-recipes', {
+      templateUrl: 'app/edit-recipes/edit-recipes.html',
+      controller: 'EditRecipesController'
+    })
+    when('/shopping-list', {
+      templateUrl: 'app/shopping-list/shopping-list.html',
+      controller: 'ShoppingListController'
+    })
+    .otherwise('/')
 });
