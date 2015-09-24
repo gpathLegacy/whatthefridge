a6deae301e7e5202abc3c60aea72ddf4e7ec073c
Jeff Berman <jeffuberman@gmail.com>
2015-09-24 15:12:12 -0500

    set up mocha test environment for backend

---
+++ specs/server/serverSpec.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var request = require('request');
+
+describe("Server", function(){  
+  it("Signup returns a token and a userid", function(done){ 
+    var options = {
+      'method': 'POST',
+      'uri': 'http://127.0.0.1:1337/api/users/signup',
+      'json': {
+        'username': 'Svnh',
+        'password': 'Svnh'
+      }
+    };
+
+    request(options, function(error, res, body){
+      expect(body).to.have.property("token")
+      expect(body).to.have.property("userid")
+      done()
+    }) 
+  });
+}); 
