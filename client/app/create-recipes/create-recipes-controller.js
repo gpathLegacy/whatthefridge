7e66b8d302bb68f0d519dee1e0c7ed9cc627ce15
Jbays <jjbaize@gmail.com>
2015-09-23 19:11:32 -0500

    change tab to spaces

--- client/app/create-recipes/create-recipes-controller.js
+++ client/app/create-recipes/create-recipes-controller.js
@@ -1,34 +1,34 @@
 angular.module('wtf.create-recipes', [])
-	.controller('CreateRecipesController', function($scope, $window, $Auth) {
+  .controller('CreateRecipesController', function($scope, $window, Auth, Recipe) {
 
-		$scope.recipe = {};
+    $scope.recipe = {};
 
-		$scope.$watch(Auth.isAuth, function(authed) {
-			if (authed) {
-				$location.path('/create-recipes');
-			} else {
-				$location.path('/');
-			}
-		}, true);
+    $scope.$watch(Auth.isAuth, function(authed) {
+      if (authed) {
+        $location.path('/create-recipes');
+      } else {
+        $location.path('/');
+      }
+    }, true);
 
-		$scope.root = 
+    $scope.root = 
 
-		$scope.submit = function(ingredient) {
-			Recipe.createRecipe(recipe).then(function(data, err) {
-				if (err) console.log(err);
-				$scope.getData();
-				$scope.recipe = {};
-			});
-		};
+    $scope.submit = function(ingredient) {
+      Recipe.createRecipe(recipe).then(function(data, err) {
+        if (err) console.log(err);
+        $scope.getData();
+        $scope.recipe = {};
+      });
+    };
 
-		$scope.getData = function() {
-			Auth.getAllData()
-				.then(function(data) {
-					$scope.recipe = data.slice(1);
-				})
-				.catch(function(error){
-					console.err(error)
-				});
-		};
-		$scope.getData();
-	})
+    $scope.getData = function() {
+      Auth.getAllData()
+        .then(function(data) {
+          $scope.recipe = data.slice(1);
+        })
+        .catch(function(error){
+          console.err(error)
+        });
+    };
+    $scope.getData();
+  })
