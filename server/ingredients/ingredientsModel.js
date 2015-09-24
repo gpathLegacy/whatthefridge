f95193ad2c33bf85e98f56edfac9a9372457a253
mishasaggi <monisha.saggi@gmail.com>
2015-09-23 19:58:46 -0500

    Add skeleton model methods and psuedocode

---
+++ server/ingredients/ingredientsModel.js
@@ -0,0 +1,19 @@
+/* database models
+exports model methods along with the connection object
+
+*/
+
+
+module.exports = function(knex) {
+    return {
+      //fetch ingredient details for an ingredient id
+      getIngredient: function(userId) {
+
+      },
+      //for a new recipe, add ingredients sent by the recipes model
+      addIngredient: function(title) {
+
+      }
+      }
+    }
+}
