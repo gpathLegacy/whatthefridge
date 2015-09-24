500cfe84b814ed262d12b700d9303fa1c34a56fd
mishasaggi <monisha.saggi@gmail.com>
2015-09-24 15:11:30 -0500

    Add recipe methods for creating and editing. Add method for adding to mapping table.

--- server/recipes/recipesModel.js
+++ server/recipes/recipesModel.js
@@ -3,9 +3,8 @@ exports model methods along with the connection object
 getRecipes - to refresh the /recipes page and render all recipes for a particular user
 createRecipe - to add a new recipe to the user's collection
 editRecipe - to edit an existing recipe from the user's collection
+addRecipeMapping - add to the mapping table, representing the many to many relationship b/w recipes and ingredients
 */
-
-
 module.exports = function(knex) {
     return {
       //fetch all recipes for a specific user. call user model method.
@@ -19,24 +18,39 @@ module.exports = function(knex) {
             'user_id': userId
           })
       },
-
       //create a new recipe. insert corresponding recipe, ingredients and mapped entries. 
-      //insert into recipes, get the id returned by the insert
+      //insert into recipes, get the ids returned by the insert
       //insert into ingredients, get the ids returned by the insert
       //create the mapping table entries
-      createRecipe: function(title) { //take ingredients from the argument array.
+      createRecipe: function(title) {
+        //handle the mapping insert on the server side instead of using currval() or lastval()
         return knex('recipes')
           .insert({
-            'title': title,
-          });
-
+            'title': title
+          })
       },
-      
       //edit existing recipes. fetch associated data. update query for the recipe, mapped table.
-      editRecipe: function(recipeId) { //id is stored on the client's side but is invisible.
+      editRecipe: function(recipeId, title) { //only the title and ingredients need to be changed. mapping remains the same
         return knex('recipes')
+          .where('id', '=', recipeId)
+          .update({
+            'title': title
+          })
+      },
+      addRecipeMapping: function(recipeId, ingredientId) { //adds foreign keys to the recipe-ingredient map table
+        //version 1: method receives one ingredient id at a time
+        return knex('recipes_ingredients')
+          .insert({
+            'recipe_id': recipeId,
+            'ingredient_id': ingredientId
+          })
 
-
+        // //version 2: method recieves all ingredients for a recipe at once
+        // //ingredientId is an array. Use a loop to insert each?
+        // return knex('recipes_ingredients')
+        //   .insert({
+        //   })
       }
     }
 }
+
