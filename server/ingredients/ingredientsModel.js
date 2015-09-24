dc842fdebea164f4b7bd3d5eef38b9d3d367f148
mishasaggi <monisha.saggi@gmail.com>
2015-09-24 15:07:37 -0500

    Add method for adding ingredients.

--- server/ingredients/ingredientsModel.js
+++ server/ingredients/ingredientsModel.js
@@ -8,11 +8,20 @@ module.exports = function(knex) {
     return {
       //fetch ingredient details for an ingredient id
       getIngredient: function(userId) {
-
+        return knex('')
+          .
       },
       //for a new recipe, add ingredients sent by the recipes model
-      addIngredient: function(title) {
-
+      addIngredient: function(name, price) {
+        //version 1: receiving one ingredient at a time
+        var price = price || 0;
+        return knex('ingredients')
+          .insert({
+            'name': name,
+            'price': price
+          })
+        //version 2: receiving multiple arguments and sending one promise back
+        //var slicedIng = Array.prototype.slice.call(arguments, 1);
       }
       }
     }
