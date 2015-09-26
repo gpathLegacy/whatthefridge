33bd55ffab1c1134935ba76d4c1e3be926f38ba5
josh-stevens <jcstevens12@outlook.com>
2015-09-25 19:42:37 -0500

    Fix recipe controller

--- server/recipes/recipesController.js
+++ server/recipes/recipesController.js
@@ -4,7 +4,6 @@ module.exports = function(Recipes, Ingredients) {
     createRecipe: function(req, res) {
       var recipeName = req.body.name;
       var ingredients = req.body.ingredients;
-      var ingredientIDs = [];
 
       // Create the recipe in the recipes table
       Recipes.createRecipe(recipeName).then(function(id) {
@@ -12,9 +11,20 @@ module.exports = function(Recipes, Ingredients) {
 
         // Get ingredient IDs that already exist, or add new Ingredients
         for (var i = 0; i < ingredients.length; i++) {
-          Ingredients.addIngredient(ingredients[i]).then(function(id) {
-            Recipes.addRecipeMapping(recipeID, id[0]).then(function() {});
-          });
+
+          // Tricky way to pass i into promise scope
+          (function(i) {
+            Ingredients.getIngredientByName(ingredients[i]).then(function(row){
+              if (row.length) {
+                Recipes.addRecipeMapping(recipeID, row[0].id).then(function(){});
+              }
+              else {
+                Ingredients.addIngredient(ingredients[i]).then(function(id) {
+                  Recipes.addRecipeMapping(recipeID, id[0]).then(function(){});
+                })
+              }
+            })
+          })(i);
 
         }
       });
