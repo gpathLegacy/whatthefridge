a194cead4a33aaac9213ca41b7fd3e18ded5d8ec
mishasaggi <monisha.saggi@gmail.com>
2015-09-26 11:26:08 -0500

    Add basic structure and comments.

---
+++ client/app/edit-recipes/edit-recipes-controller.js
@@ -0,0 +1,21 @@
+angular.module('wtf.edit-recipes', [])
+  .controller('EditRecipesController', function($scope, $window, $location, Recipes){
+
+    //local variable storing current recipe values
+
+    //add a new ingredient
+
+    //remove an ingredient
+
+    //change recipe title
+    
+
+  });
+
+
+
+  /*
+    get passed in a recipe
+    generate a form with the existing values populated
+    have a submit button for the form
+  */
