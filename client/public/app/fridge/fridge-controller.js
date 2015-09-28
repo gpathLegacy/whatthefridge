e51eebb2da085ed0a24dc5b46f5afc4cd7fb2839
Eric <hoang.e.nguyen@gmail.com>
2015-09-28 16:36:50 -0500

    Add fridge template and mock data

--- client/public/app/fridge/fridge-controller.js
+++ client/public/app/fridge/fridge-controller.js
@@ -1,4 +1,10 @@
 angular.module('wtf.fridge',[])
   .controller('FridgeController',function($scope,$http, $location) {
-    $scope.cold = "I am very cold"
+    // $scope.cold = "I am very cold"
+    $scope.testData = [
+      {ingredient: "meat", price: "$5", quantity: 3},
+      {ingredient: "pasta", price: "$2", quantity: 4},
+      {ingredient: "chocolate", price:"$3", quantity: 5},
+      {ingredient: "flour", price: "$2", quantity: 3}
+    ]
 });
