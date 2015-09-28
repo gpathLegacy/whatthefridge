ebc7864111f982be21e04120f80224cd2614d212
mishasaggi <monisha.saggi@gmail.com>
2015-09-28 16:42:22 -0500

    Add Josh's refactoring to dashboard and services. Selected recipes array now lives in the factory.

--- client/public/app/services/services.js
+++ client/public/app/services/services.js
@@ -3,18 +3,19 @@ angular.module('wtf.services', [])
     var createRecipe = function(recipe){
       return $http.post('/api/recipes/createRecipe', recipe);
     }
-    //post request to save edited recipe
     var editRecipe = function(recipe){
       return $http.post('/api/recipes/editRecipe', recipe);
     }
     var getRecipes = function() {
       return $http.post('/api/recipes/getRecipes');
     }
-    //function to get all recipes
-    //http get request for dashboard 
+    var  selectedRecipes = [];
+
     return {createRecipe:createRecipe, 
             editRecipe:editRecipe, 
-            getRecipes:getRecipes};
+            getRecipes:getRecipes,
+            selectedRecipes:selectedRecipes
+            };
   })  
   .factory('Navbar', function($http) {
     var isLoggedIn = function() {
