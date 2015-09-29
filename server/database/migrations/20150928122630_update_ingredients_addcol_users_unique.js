d17cfe308c12d79cf4fd596adb368a0b172c2a0b
Eric Nguyen <hoang.e.nguyen@gmail.com>
2015-09-29 14:38:13 -0500

    Fix spacing

--- server/database/migrations/20150928122630_update_ingredients_addcol_users_unique.js
+++ server/database/migrations/20150928122630_update_ingredients_addcol_users_unique.js
@@ -2,13 +2,13 @@
 exports.up = function(knex, Promise) {
   var users_update = knex.schema.table('users', function(table){
     table.unique('username');
-    })
+  })
 
   var ingredients_update = knex.schema.table('ingredients', function(table){
     table.integer('user_id').references('id').inTable('users');   
     table.dropUnique('name');
     table.unique(['user_id', 'name']); 
-      })
+  })
 
   return Promise.all([ingredients_update, users_update]);
 };
