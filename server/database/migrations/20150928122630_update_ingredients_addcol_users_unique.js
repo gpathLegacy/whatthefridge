0ceaed762c38768ba5610e812a14be0097046aa8
mishasaggi <monisha.saggi@gmail.com>
2015-09-29 11:09:56 -0500

    Add unique constraints to user specific ingredients and username.

--- server/database/migrations/20150928122630_update_ingredients_addcol_users_unique.js
+++ server/database/migrations/20150928122630_update_ingredients_addcol_users_unique.js
@@ -1,7 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
-  
+  return knex.schema
+    .table('ingredients', function(table){
+      .table.integer('user_id').references('id').inTable('users')   
+      .table.unique(['user_id', 'name']) 
+      })
+    .table('users', function(table){
+      .table.unique('username')
+    })  
 };
 
 exports.down = function(knex, Promise) {
