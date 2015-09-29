f7d7a8f050e49a00689f578c9147ad5b462e511f
Eric Nguyen <hoang.e.nguyen@gmail.com>
2015-09-29 09:30:01 -0500

    Remove unnecessary line from fridge-controller

--- client/public/app/fridge/fridge-controller.js
+++ client/public/app/fridge/fridge-controller.js
@@ -1,6 +1,5 @@
 angular.module('wtf.fridge',[])
   .controller('FridgeController',function($scope,$http, $location) {
-    // $scope.cold = "I am very cold"
     $scope.testData = [
       {ingredient: "meat", price: "$5", quantity: 3},
       {ingredient: "pasta", price: "$2", quantity: 4},
