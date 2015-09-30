6a5ce9b1a9fec2b96f5d5df425566485f0ef3b7d
josh-stevens <jcstevens12@outlook.com>
2015-09-30 10:36:57 -0500

    Cleaned up server comments

--- server/server.js
+++ server/server.js
@@ -42,7 +42,9 @@ app.use(passport.session());
 
 // =============== ROUTING =================
 
+// -----------------------------------------
 // Secured Routes --------------------------
+
 var auth = function(req, res, next){
   if (!req.isAuthenticated())
     res.send(401);
@@ -67,7 +69,9 @@ app.get('/app/shopping-list/shopping-list.html', auth, function(req, res) {
 app.get('/app/fridge/fridge.html', auth, function(req, res) {
   res.sendFile(path.resolve(__dirname + '/../client/app/fridge/fridge.html'));
 });
+
 // -----------------------------------------
+// API routes ------------------------------
 
 var usersRouter = express.Router();
 var recipesRouter = express.Router();
@@ -79,7 +83,7 @@ app.use('/api/recipes', recipesRouter); //use recipe router
 app.use('/api/ingredients', ingredientsRouter); // use ingredient router
 app.use('/api/fridge', fridgeRouter);
 
-// inject our routers into their respective route files
+// inject our routers and models into their respective route files
 require('./users/usersRoutes.js')(usersRouter, passport);
 require('./recipes/recipesRoutes.js')(recipesRouter, Recipes, Ingredients);
 require('./ingredients/ingredientsRoutes.js')(ingredientsRouter, Ingredients);
