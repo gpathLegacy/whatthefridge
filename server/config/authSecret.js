53db089f5b077e20def36cb06a961e5f15e0ad75
MKS-students <mks.students@hackreactor.com>
2015-10-05 11:24:34 -0500

    Change gitignore to add authSecret.js

---
+++ server/config/authSecret.js
@@ -0,0 +1,22 @@
+module.exports = {
+ 'facebookAuth' : {
+   clientID: process.env.FACEBOOK_CLIENT_ID,
+   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+   callbackURL: process.env.FACEBOOK_CALLBACK_URL
+ },
+ 'twitterAuth' : {
+   consumerKey: process.env.TWITTER_CONSUMER_KEY,
+   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+   callbackURL: process.env.TWITTER_CALLBACK_URL
+ },
+ 'instagramAuth' : {
+   clientID: process.env.INSTAGRAM_CLIENT_ID,
+   clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+   callbackURL: process.env.INSTAGRAM_CALLBACK_URL
+ },
+ 'googleAuth' : {
+   clientID: process.env.GOOGLE_CLIENT_ID,
+   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+   callbackURL: process.env.GOOGLE_CALLBACK_URL
+ }
+}
