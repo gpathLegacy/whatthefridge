b1e3e7f78ced5b0a42bfbc2bbd4ad2305bab42e4
Jeff Berman <jeffuberman@gmail.com>
2015-10-13 10:35:16 -0500

    add dynamic textarea for recipe instructions in create recipe

--- client/public/app/create-recipes/create-recipes-controller.js
+++ client/public/app/create-recipes/create-recipes-controller.js
@@ -38,4 +38,10 @@ angular.module('wtf.create-recipes', [])
           });
       }
     };
+
+    $scope.autoExpand = function(e) {
+      var element = ( typeof e === 'object' ? e.target : document.getElementById(e) );
+      var scrollHeight = element.scrollHeight -60; // replace 60 by the sum of padding-top and padding-bottom
+      element.style.height =  scrollHeight + "px";    
+    };
   }]);
