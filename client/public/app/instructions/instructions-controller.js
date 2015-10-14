1478864baaf308232f1bc4fbc7cafb1aaad109c5
Jeff Berman <jeffuberman@gmail.com>
2015-10-14 10:15:53 -0500

    render instructions.html

---
+++ client/public/app/instructions/instructions-controller.js
@@ -0,0 +1,5 @@
+angular.module('wtf.instructions', [])
+  .controller('InstructionsController', ["$scope", "$window", "$location", "Recipes", function($scope, $window, $location, Recipes) {
+
+    
+  }]);
