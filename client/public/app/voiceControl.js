2ce4eb77437d7b4e874dd0f0757def26b871bec5
Eric <hoang.e.nguyen@gmail.com>
2015-10-14 12:36:23 -0500

    Add show to display recipe cards

--- client/public/app/voiceControl.js
+++ client/public/app/voiceControl.js
@@ -1,46 +1,53 @@
-      if (annyang && window.location.href.split('/').pop() !== "") {
-        // Let's define a command.
-        var goTo = function(view) {
+if (annyang && window.location.href.split('/').pop() !== "") {
+  // Let's define a command.
+  var goTo = function(view) {
 
-          //handle hyphenated paths
-          //if (view==='saved lists'){
-          //}
-          //
-          var endPointStart = window.location.href.lastIndexOf('/');
-          var basicPath = window.location.href.slice(0, endPointStart +1);
+    //handle hyphenated paths
+    //if (view==='saved lists'){
+    //}
+    //
+    var endPointStart = window.location.href.lastIndexOf('/');
+    var basicPath = window.location.href.slice(0, endPointStart +1);
 
-          window.location.href = basicPath + view;
-        }
+    window.location.href = basicPath + view;
+  }
 
-        var logout = function(){
-          $("#logout").click();
-          anyang.abort();
-        }
-        
-        var scroll = function(position){
-          if(position === "top"){
-            $("html, body").animate({ scrollTop: 0 }, "slow")
-          }
-          if(position === "bottom"){
-            $("html, body").animate({ scrollTop: $(document).height()-$(window).height() })
-          }
-          if(position === "down"){
-            $('html, body').animate({scrollTop: '+=150px'}, 800)
-          }
-          if(position === "up"){
-            $('html, body').animate({scrollTop: '-=150px'}, 800)
-          }
-        }
-        
-        var commands = {
-          'go to *view': goTo,
-          'scroll *position': scroll,
-          'log out': logout,
-        };
+  var logout = function(){
+    $("#logout").click();
+    anyang.abort();
+  }
+  
+  var scroll = function(position){
+    if(position === "top"){
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+    }
+    if(position === "bottom"){
+      $("html, body").animate({ scrollTop: $(document).height()-$(window).height() });
+    }
+    if(position === "down"){
+      $('html, body').animate({scrollTop: '+=150px'}, 800);
+    }
+    if(position === "up"){
+      $('html, body').animate({scrollTop: '-=150px'}, 800);
+    }
+  }
+  
+  //click activator for recipe card
+  var show = function(recipe){
+    var idIfied = "#"+ recipe
+    $(idIfied).click();
+  }
 
-        // Add our commands to annyang
-        annyang.addCommands(commands);
+  var commands = {
+    'go to *view': goTo,
+    'scroll *position': scroll,
+    'log out': logout,
+    'show *recipe': show
+  };
 
-        // Start listening.
-        annyang.start();
-      }
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening.
+  annyang.start();
+}
